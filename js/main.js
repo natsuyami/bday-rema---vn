bbe807dd76c1a7327831a29e2b72a986b8224d08
ryan gabriel bunquin <zryangabriel.bunquin@mynt.xyz>
2021-04-26 22:47:41 +0800

    Add Loader for story

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-document.addEventListener('click', musicPlay);
 function musicPlay() {
+    $("#standByMeAudio")[0].volume = 0;
+    $("#standByMeAudio").animate({volume: 0.7}, 3000);
     document.getElementById('standByMeAudio').play();
-    document.removeEventListener('click', musicPlay);
 }
 
-musicPlay();
+$(document).ready(function() {
+    $("#standByMeAudio")[0].volume = 0;
+    $("#standByMeAudio").animate({volume: 0.7}, 3000);
+        
+    setTimeout(function(){
+        $("#pageLoader").fadeOut(300, function() { $(this).remove(); });
+    }, 4000);
+
+    var ht = $("#textHolder p").height();
+    $("#textHolder p").delay(4000).animate({width:'toggle', height: ht}, 3000);
+});
